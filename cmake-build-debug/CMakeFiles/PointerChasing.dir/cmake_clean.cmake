85ded25377d6339bbf85193699f75a9ce6bfa186
Massimo Imparato <33756597+Massimoic@users.noreply.github.com>
2020-03-25 23:22:39 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/PointerChasing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointerChasing.dir/main.cpp.obj"
+  "PointerChasing.exe"
+  "PointerChasing.exe.manifest"
+  "PointerChasing.pdb"
+  "libPointerChasing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointerChasing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PointerChasing.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PointerChasing.
+# This may be replaced when dependencies are built.
